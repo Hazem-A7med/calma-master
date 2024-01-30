d090fc8c2909601590d2cadf7db08998058f99d6
Hazem-A7med <elnagarhazem.official@gmail.com>
2024-01-30 23:09:54 +0200

    create post

--- lib/presentation/screen/BottombarScreen/MainPage/creat_post_screen.dart
+++ lib/presentation/screen/BottombarScreen/MainPage/creat_post_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nadek/core/utils/app_colors.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nadek/sheard/style/ColorApp.dart';
 
 import '../../../../sheard/constante/cache_hleper.dart';
 
@@ -22,10 +23,14 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
           style: TextStyle(fontSize: 18),
         ),
         backgroundColor: Colors.transparent,
-        actions:  <Widget>[
-          MaterialButton(onPressed: () {
-
-          },child: Text('Post',style: TextStyle(fontSize: 18,color: Colors.white),),)
+        actions: <Widget>[
+          MaterialButton(
+            onPressed: () {},
+            child: const Text(
+              'Post',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          )
         ],
       ),
       backgroundColor: AppColors.scaffold,
@@ -44,20 +49,68 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
                         width: 5,
                       ),
                       CircleAvatar(
+                          radius: 23,
                           backgroundImage: NetworkImage(
                               CacheHelper.getString('photo')
                                   .toString()
                                   .replaceAll('\'', ''))),
                       const SizedBox(
-                        width: 5,
+                        width: 10,
                       ),
                       Text(
                         CacheHelper.getString('username') ?? '',
-                        style: const TextStyle(color: Colors.white),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 18),
                       ),
                     ],
                   ),
                 ),
+              ),
+              TextField(
+                cursorColor: ColorApp.darkRead,
+                style: TextStyle(
+                    fontSize: 18, color: Colors.white.withOpacity(.6)),
+                textDirection: TextDirection.rtl,
+                maxLines: null,
+                decoration: InputDecoration(
+                    hintText:
+                        'ما الذي يدور في ذهنك,${CacheHelper.getString('username')} ؟',
+                    hintTextDirection: TextDirection.rtl,
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white.withOpacity(.3),
+                    ),
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    )),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.15),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20))),
+                    child: Row(
+                      children: [
+                        Text('صورة / فيديو',
+                            style: TextStyle(
+                                color: Colors.white.withOpacity(.7),
+                                fontSize: 22)),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Image.asset('assets/icons/image.png', height: 30),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
